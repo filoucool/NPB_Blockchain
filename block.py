12b59f912e56b4d9c20bd251b34c73e3be09a91f
Félix Chenette-Stewart <felix@nexumnet.com>
2021-04-22 19:55:56 -0400

    Block constructor

---
+++ block.py
@@ -0,0 +1,6 @@
+class Block:
+        def __init__(self, previous_block_hash, data, timestamp):
+            self.previous_block_hash = previous_block_hash
+            self.data = data
+            self.timestamp = timestamp
+            self.hash self.get_hash
