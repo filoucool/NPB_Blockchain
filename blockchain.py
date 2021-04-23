16134899a494c879e9e2d1d39af262650a56cf6d
Félix Chenette-Stewart <felix@nexumnet.com>
2021-04-22 22:01:17 -0400

    blockchain

--- blockchain.py
+++ blockchain.py
@@ -1,9 +1,9 @@
 from block import Block
 import datetime
 import sys
-sys.stdout = open("test.txt", "w")
+sys.stdout = open("output.txt", "w")
 
-num_blocks_to_add = 100
+num_blocks_to_add = 1000000
 
 block_chain = [Block.create_NPB_block()]
 
@@ -15,8 +15,6 @@ for i in range(1, num_blocks_to_add):
     block_chain.append(Block(block_chain[i-1].hash,
                              "Block number %d" % i,
                              datetime.datetime.now()))
-    print("Block #%d created." % i)
     print("Hash: %s" % block_chain[-1].hash)
-
 
 sys.stdout.close()
