d520676f6094c9f8176dd4305cb2f2239ac49b3d
Félix Chenette-Stewart <felix@nexumnet.com>
2021-04-22 20:40:21 -0400

    block

--- block.py
+++ block.py
@@ -1,22 +1,24 @@
-import hashlib
+
 import datetime
+import hashlib
+
 
 class Block:
-        def __init__(self, previous_block_hash, data, timestamp):
-            self.previous_block_hash = previous_block_hash
-            self.data = data
-            self.timestamp = timestamp
-            self.hash = self.get_hash
-        
-        @staticmethod
-        def create_NPB_block():
-            return Block("0", "0", datetime.datetime.now())
+    def __init__(self, previous_block_hash, data, timestamp):
+        self.previous_block_hash = previous_block_hash
+        self.data = data
+        self.timestamp = timestamp
+        self.hash = self.get_hash()
 
-        def get_hash(self):
-            header_bin = (str(self.previous_block_hash)+
-                        str(self.data)+
-                        str(self.timestamp)).encode()
-            
-            inner_hash = hashlib.sha256(header_bin).hexdigest().encode()
-            outer_hash = hashlib.sha256(inner_hash).hexdigest()
-            return outer_hash
+    @staticmethod
+    def create_NPB_block():
+        return Block("0", "0", datetime.datetime.now())
+
+    def get_hash(self):
+        header_bin = (str(self.previous_block_hash) +
+                      str(self.data) +
+                      str(self.timestamp))
+
+        inner_hash = hashlib.sha256(header_bin.encode()).hexdigest().encode()
+        outer_hash = hashlib.sha256(inner_hash).hexdigest()
+        return outer_hash
--- blockchain.py
+++ blockchain.py
@@ -1,4 +1,22 @@
 from block import Block
+import datetime
+import sys
+sys.stdout = open("test.txt", "w")
 
-b1 = Block.create_NPB_block()
-print(b1.hash)
+num_blocks_to_add = 100
+
+block_chain = [Block.create_NPB_block()]
+
+print("NPB block has been created.")
+
+print("Hash: %s" % block_chain[0].hash)
+
+for i in range(1, num_blocks_to_add):
+    block_chain.append(Block(block_chain[i-1].hash,
+                             "Block number %d" % i,
+                             datetime.datetime.now()))
+    print("Block #%d created." % i)
+    print("Hash: %s" % block_chain[-1].hash)
+
+
+sys.stdout.close()
