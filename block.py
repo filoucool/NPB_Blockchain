3936792e3bfae8930582eb12079468829b8e5470
Félix Chenette-Stewart <felix@nexumnet.com>
2021-04-22 20:09:01 -0400

    constructor

--- block.py
+++ block.py
@@ -1,10 +1,17 @@
+import hashlib
+import datetime
+
 class Block:
         def __init__(self, previous_block_hash, data, timestamp):
             self.previous_block_hash = previous_block_hash
             self.data = data
             self.timestamp = timestamp
-            self.hash self.get_hash
+            self.hash = self.get_hash
         
+        @staticmethod
+        def create_NPB_block():
+            return Block("0", "0", datetime.datetime.now())
+
         def get_hash(self):
             header_bin = (str(self.previous_block_hash)+
                         str(self.data)+
---
+++ blockchain.py
@@ -0,0 +1,4 @@
+from block import Block
+
+b1 = Block.create_NPB_block()
+print(b1.hash)
